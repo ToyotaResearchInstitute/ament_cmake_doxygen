5ce4b79b6ccae7c8be08097f817f22d585f1e0bf
Michel Hidalgo <michel@ekumenlabs.com>
2019-10-02 13:41:15 -0300

    Address peer review comments.
    Signed-off-by: Michel Hidalgo <michel@ekumenlabs.com>

--- cmake/ament_doxygen_generate.cmake
+++ cmake/ament_doxygen_generate.cmake
@@ -1,4 +1,42 @@
+# Copyright 2019 Toyota Research Institute
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
+#
+# Generate documentation using Doxygen.
+#
+# Provided Doxygen configuration defaults expect C++ sources. This macro is capable of
+# linking against upstream packages' documentation and expose generated documentation
+# for downstream packages to link against.
+#
+# :param CONFIG_OVERLAY: Path to a Doxyfile overlay file, which may be a template if
+#                        the '.in' suffix is present. Defaults to none.
+# :type CONFIG_OVERLAY: string
+# :param PROJECT_NAME: Doxygen project name. Defaults to the current CMake project name.
+# :type PROJECT_NAME: string
+# :param INPUT_DIRECTORY: Path to directory to crawl looking for sources.
+#                         Defaults to the current directory in the CMake project source
+#                         tree (i.e. the directory this macro was called at).
+# :type INPUT_DIRECTORY: string
+# :param OUTPUT_DIRECTORY: Path to directory to generate output into.
+#                          Defaults to the ament_cmake_doxygen/${PROJECT_NAME}
+#                          directory in the current CMake project build tree.
+# :type OUTPUT_DIRECTORY: string
+# :param DEPENDENCIES: List of project dependencies. Must be known by ament.
+# :type DEPENDENCIES: list of strings
+#
+# @public
+#
 function(ament_doxygen_generate)
   cmake_parse_arguments(
     args
@@ -27,6 +65,10 @@ function(ament_doxygen_generate)
   endif()
   set(OUTPUT_DIRECTORY "${args_OUTPUT_DIRECTORY}")
 
+  if (DOXYGEN_DOT_FOUND)
+    set(HAVE_DOT YES)
+    get_filename_component(DOT_PATH "${DOXYGEN_DOT_EXECUTABLE}" DIRECTORY)
+  endif()
 
   foreach(dep ${args_DEPENDENCIES})
     ament_index_has_resource(has_tag doxygen_tags "${dep}")
@@ -48,7 +90,7 @@ function(ament_doxygen_generate)
     string_ends_with("${args_CONFIG_OVERLAY}" ".in" is_template)
     if(is_template)
       get_filename_component(overlay_filename "${args_CONFIG_OVERLAY}" NAME)
-      # cut of .in extension
+      # cut off .in extension
       string(LENGTH "${overlay_filename}" length)
       math(EXPR offset "${length} - 3")
       string(SUBSTRING "${overlay_filename}" 0 ${offset} overlay_filename)
